e19659602b07c33f7bf1db26eedea504794b9aaf
Germán C.R. <germanux@gmail.com>
2026-08-05 19:13:36 +0200

    chore(drive): import 5 updates and 0 deletions

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,14 +1,11 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
-import { BACKEND_URL } from './core/config/backend-url.token';
-import { environment } from '../environments/environment';
+import { ApplicationConfig } from '@angular/core';
 
-import { routes } from './app.routes';
+import { environment } from '../environments/environment';
+import { BACKEND_URL } from './core/config/backend-url.token';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
     provideHttpClient(),
     { provide: BACKEND_URL, useValue: environment.backendUrl }
   ]
